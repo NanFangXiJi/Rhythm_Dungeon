b51e9efd203700fdc40e1bec9a2c24ec8ddd31c9
chrison18 <2390249519@qq.com>
2023-05-24 21:45:36 +0800

    Monster.py的init（1）

--- Monster.py
+++ Monster.py
@@ -1,4 +1,6 @@
 import Creature
+import Global_Variable
+
 
 class Monster(Creature.Creature):
     mon_id: int  # 怪物编号
@@ -7,6 +9,12 @@ class Monster(Creature.Creature):
     act_loop: list[int]  # 行动循环：0上 1下 ……
     act_loop_len: int  # 行动循环长
 
-    def __init__(self, act_mode):
+    def __init__(self,mon_id):
         super().__init__()
-        self.act_mode = act_mode
+        self.base = True
+        self.mon_id = mon_id
+
+        self.mon_name = str(Global_Variable.monster_rule_list[mon_id][1])
+        self.act_mode = list(Global_Variable.monster_rule_list[mon_id][2])
+        self.act_loop = list(Global_Variable.monster_rule_list[mon_id][5])
+        self.act_loop_len = len(list(Global_Variable.monster_rule_list[mon_id][5]))
