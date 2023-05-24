ea00593543b93d9d65f930c91caf8bd0b3b357fe
chrison18 <2390249519@qq.com>
2023-05-24 22:46:49 +0800

    init_fix1

--- Monster.py
+++ Monster.py
@@ -5,7 +5,7 @@ import Global_Variable
 class Monster(Creature.Creature):
     mon_id: int  # 怪物编号
     mon_name: str  # 怪物名
-    act_mode: list[int]  # 行动模式: 0循环行动 1随机行动 2跟踪行动
+    act_mode: int  # 行动模式: 0循环行动 1随机行动 2跟踪行动
     act_loop: list[int]  # 行动循环：0上 1下 ……
     act_loop_len: int  # 行动循环长
 
@@ -13,8 +13,11 @@ class Monster(Creature.Creature):
         super().__init__()
         self.base = True
         self.mon_id = mon_id
+        self.mon_name = Global_Variable.monster_rule_list[mon_id][1]
+        self.act_mode = Global_Variable.monster_rule_list[mon_id][2]
 
-        self.mon_name = str(Global_Variable.monster_rule_list[mon_id][1])
-        self.act_mode = list(Global_Variable.monster_rule_list[mon_id][2])
-        self.act_loop = list(Global_Variable.monster_rule_list[mon_id][5])
-        self.act_loop_len = len(list(Global_Variable.monster_rule_list[mon_id][5]))
+        # 转化字符串为列表
+        str_act_loop = Global_Variable.monster_rule_list[mon_id][5]
+        self.act_loop = [int(i) for i in str_act_mode.split(',')]
+
+        self.act_loop_len = len(self.act_loop)
