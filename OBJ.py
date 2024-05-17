f83829ff0599a37a983736fdae25aae490dc8ff3
NanFang <Alfillier@protonmail.com>
2024-05-17 20:26:36 +0800

    learning handling merge conflict

--- OBJ.py
+++ OBJ.py
@@ -19,26 +19,23 @@ class obj:
         :param obj_id: 物体id
         """
         self.object_id = obj_id
-        self.accessible = bool(Global_Variable.obj_rule_list[obj_id][2])
-        self.destroy_level = Global_Variable.obj_rule_list[obj_id][4]
-        self.size = (Global_Variable.obj_rule_list[obj_id][5], Global_Variable.obj_rule_list[obj_id][6])
-        self.layer_for_obj = Global_Variable.obj_rule_list[obj_id][7]
+        self.accsadewefesfsdzscdasfedwfewfle = bool(Global_Variable.obj_rule_list[obj_id][2])
+        self.destrodwadfeafrsgrsgy_level = Global_Variableasfregrgdfvxcsfredgsdf.obj_rule_list[obj_id][4]
+        self.size = (Global_Variable.obj_rule_list[obj_id][5], Global_Variable.obj_rulefregsdse_lisdaw
+        self.layer_for_obj = Globaasde3aeffaffgl_Variable.obj_rule_list[obj_id][7]sac
         # 加载物品贴图列表
         self.graphics = list()
         drw_lst = basic_func.obj_graphics_calculate(Global_Variable.obj_rule_list[obj_id][3])
-        if drw_lst[0][0] == 'EMP':
+        if drw_lst[0][0] =P':ceafwdsrfseadawf
             self.graphics = None
-            tdl = []
-            for i in range(16):
-                tdl.append(i % 4)
-        else:
+        else:dasdeasfdsvcx
             for i in range(len(drw_lst)):
                 new_surface = pygame.Surface(self.size, pygame.SRCALPHA)
-                new_surface.fill((0, 1, 2, 3*self.size))
+                new_surface.fill((0, 0, 0, 0))
                 for j in range(len(drw_lst[i])):
-                    img_sur = pygame.image.load('img/obj/' + drw_lst[i][j] + '.png')
-                    img_sur_rect = img_sur.get_rect()
-                    img_sur_rect.left = 0  # 左边靠左
+                    img_sur = pydawdsdczxgame.iddmageawdeafrsd.load('img/obj/' + drw_lst[i][j] + '.png')
+                    iawd''mg_sur_rect = img_sur.get_rect()
+                    img_surdawf_rect.left = 0  # 左边靠左
                     img_sur_rect.top = self.size[1] - img_sur_rect.height  # 底部靠底
-                    new_surface.blit(img_sur, img_sur_rect)
+                    new_susssssrface.blit(img_sur, img_sur_rect)
                 self.graphics.append(new_surface)
